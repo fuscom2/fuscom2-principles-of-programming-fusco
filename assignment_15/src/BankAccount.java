3675c5050f90bd317a6617876bd768910918388a
fuscom2 <fuscom2@newpaltz.edu>
2026-04-28 16:22:37 -0400

    Add files via upload

---
+++ assignment_15/src/BankAccount.java
@@ -0,0 +1,51 @@
+package bankaccount;
+
+public class BankAccount {
+
+    private double balance;
+
+    // Constructor
+    public BankAccount(double initialBalance) {
+        if (initialBalance < 0) {
+            throw new IllegalArgumentException("Initial balance cannot be negative");
+        }
+        this.balance = initialBalance;
+    }
+
+    // Method to deposit an amount
+    public void deposit(double amount) throws IllegalArgumentException{
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Deposit amount must be greater than zero");
+        }
+        balance += amount;
+    }
+
+    // Method to withdraw an amount
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdraw amount must be greater than zero");
+        }
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance -= amount;
+    }
+
+    public void transfer(BankAccount account, double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Withdraw amount must be greater than zero");
+        }
+        if (balance - amount < 0) {
+            throw new IllegalArgumentException("Withdraw amount must be greater than zero");
+        }
+
+        account.deposit(amount);
+
+        balance = balance - amount;
+    }
+
+    // Method to get the current balance
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ assignment_15/src/Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    int[] arr1;
+    int[] arr2;
+    String str1;
+    String str2;
+
+    public Data() {
+        arr1 = new int[] { 0, 5, 13, 21};
+        arr2 = new int[] { 20, 21, 23 };
+
+        str1 = "Hello, world!";
+        str2 = "Hello, world!";
+    }
+}
